6e6d7ba584e17e55bf5137b815f8eef0de4f042d
barendan <barendan@gmail.com>
2021-04-20 12:41:38 -0400

    add basic navigation structure

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Posts from "../screens/Posts";
+
+const Stack = createStackNavigator();
+
+// Navigation Container hosts app navigation context
+// Stack Navigator hosts a collection of screens
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Posts" component={Posts} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
