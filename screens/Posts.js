b38cf43c521a73c824b7fbb02edebba1b7e0e28c
barendan <barendan@gmail.com>
2021-04-24 13:20:33 -0400

    setup query and subscriptions using hooks

--- screens/Posts.js
+++ screens/Posts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,28 +7,61 @@ import {
   TouchableOpacity,
   ActivityIndicator,
 } from "react-native";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 import { Ionicons } from "@expo/vector-icons";
 import Post from "../components/Post";
 
-const data = {
-  posts: [
-    {
-      id: 1,
-      title: "Hello World!",
-      link: "https://google.com",
-      imageUrl: "https://picsum.photos/600/900",
-    },
-    {
-      id: 2,
-      title: "Welcome to reddit! oontz oontz",
-      link: "https://google.com",
-      imageUrl: "https://picsum.photos/500/800",
-    },
-  ],
-};
+const POSTS_QUERY = gql`
+  query {
+    posts {
+      id
+      title
+      link
+      imageUrl
+    }
+  }
+`;
+
+const POSTS_SUBSCRIPTION = gql`
+  subscription {
+    postAdded {
+      id
+      title
+      link
+      imageUrl
+    }
+  }
+`;
 
 export default function Posts(props) {
   const { navigation } = props;
+  const { subscribeToMore, loading, error, data } = useQuery(POSTS_QUERY, {
+    variables: {},
+  });
+
+  useEffect(() => {
+    subscribeToMore({
+      document: POSTS_SUBSCRIPTION,
+      variables: {},
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+        const newPostItem = subscriptionData.data.postAdded;
+        return Object.assign({}, prev, {
+          posts: [newPostItem, ...prev.posts],
+        });
+      },
+    });
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator style={{ ...StyleSheet.absoluteFillObject }} />;
+  }
+
+  if (error) {
+    return <Text>{error.message}</Text>;
+  }
+
   const { posts } = data;
 
   return (
